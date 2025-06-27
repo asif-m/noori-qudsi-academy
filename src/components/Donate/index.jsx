ceb1911d4147133e258df5632b86d983b5b44102
Mohammed Asif <mohd.asif55@gmail.com>
2025-06-27 10:28:46 -0700

    REmoving bank details and tax payer id

--- src/components/Donate/index.jsx
+++ src/components/Donate/index.jsx
@@ -31,12 +31,10 @@ export default function Donate() {
 						<div>[Sahih Al-Bukhari]</div>
 					</div>
 					<div className="flex flex-wrap justify-center gap-6">
-						<BankDetails />
 						<ZeffyDonate />
 						<PaypalDonate />
 						<QrCode />
 					</div>
-					<div className="text-sm">EIN (Taxpayer Id) : 87-1825550</div>
 					<div>
 						{/* <LoanInfo></LoanInfo> */}
 					</div>
@@ -63,47 +61,6 @@ function QrCode() {
 	return <div className="w-64 h-64 rounded overflow-hidden shadow-lg text-sm flex items-center justify-center text-center">
 		<div>
 			<img src={QrCodeImage} alt="QrCode" className="w-48 h-48" />
-		</div>
-	</div>;
-}
-
-function BankDetails() {
-	const routingNumber = "071025661";
-	const accountNumber = "68321678";
-	const [showCopiedToClipboard, setShowCopiedToClipboard] = useState(false);
-	async function copyTextToClipboard() {
-		const textToCopy = `
-		Routing #: ${routingNumber} 
-		Account #: ${accountNumber}
-		`
-		try {
-			await navigator.clipboard.writeText(textToCopy);
-			setShowCopiedToClipboard(true);
-		} catch (err) {
-		}
-	}
-	useEffect(() => {
-		let timerId = 0;
-		if (showCopiedToClipboard) {
-			timerId = setTimeout(() => {
-				setShowCopiedToClipboard(false);
-			}, 3000)
-		}
-		return () => clearTimeout(timerId)
-	}, [showCopiedToClipboard])
-
-	return <div className="w-64 h-64 rounded overflow-hidden shadow-lg p-5 text-sm flex items-center justify-center text-center">
-		<div>
-			<div>Routing #: {routingNumber} </div>
-			<div>Account #: {accountNumber}</div>
-			<div className="py-3">
-				<button onClick={copyTextToClipboard}>
-					<svg viewBox="0 0 24 24" style={{ height: "32px", width: "32px" }}><path fill="currentColor" d="M19,21H8V7H19M19,5H8A2,2 0 0,0 6,7V21A2,2 0 0,0 8,23H19A2,2 0 0,0 21,21V7A2,2 0 0,0 19,5M16,1H4A2,2 0 0,0 2,3V17H4V3H16V1Z"></path></svg>
-				</button>
-			</div>
-			<div className="text-[10px]">
-				{showCopiedToClipboard ? "✅ Bank details copied to clipboard" : " "}
-			</div>
 		</div>
 	</div>;
 }
